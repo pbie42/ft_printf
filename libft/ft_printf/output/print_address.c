033de0f06ac2b8f8433f0ea00875f33f730503fe
Paul Bie <pbie@student.42.fr>
2017-12-13 15:49:21 +0100

    [Fix] - Precision and field width when printing address. 6 moulitest errors left.

--- libft/ft_printf/output/print_address.c
+++ libft/ft_printf/output/print_address.c
@@ -12,11 +12,12 @@
 
 #include "libftprintf.h"
 
-void				print_address_precision(t_pf_item *pfi, char *num)
+void				print_address_precision(t_pf_item *pfi, char *num, int prcsn)
 {
 	int				i;
 	int				len;
 
+	i = prcsn;
 	if (!pfi->flags->zero)
 		ft_putstr("0x");
 	i = pfi->precision;
@@ -30,29 +31,34 @@ void				print_address_precision(t_pf_item *pfi, char *num)
 	pfi->bytes += ft_strlen(num);
 }
 
-int					address_get_width(t_pf_item *pfi, char *num)
+int					address_get_width(t_pf_item *pfi, char *num, int prcsn)
 {
 	int				width;
 
 	width = 0;
+	if (pfi->precision < (int)ft_strlen(num))
+		pfi->precision = (int)ft_strlen(num);
 	if (pfi->precision >= pfi->field_w)
+	{
 		pfi->precision = 0;
+		prcsn = 0;
+	}
 	width = pfi->field_w - pfi->precision - 1;
 	if (!pfi->precision)
 		width -= ft_strlen(num);
 	return (width - 2);
 }
 
-void				print_address_field_w(t_pf_item *pfi, char *num)
+void				print_address_field_w(t_pf_item *pfi, char *num, int prcsn)
 {
 	int				i;
 	int				width;
 
 	i = 0;
-	width = address_get_width(pfi, num);
+	width = address_get_width(pfi, num, prcsn);
 	if (pfi->flags->minus)
 	{
-		print_address_precision(pfi, num);
+		print_address_precision(pfi, num, prcsn);
 		while (i++ <= width)
 			print_space_byte(pfi);
 	}
@@ -60,33 +66,35 @@ void				print_address_field_w(t_pf_item *pfi, char *num)
 	{
 		while (i++ <= width)
 		{
-			if (pfi->flags->zero)
+			if (pfi->flags->zero && i >= prcsn)
 				ft_putchar('0');
 			else
 				ft_putchar(' ');
 			pfi->bytes++;
 		}
-		print_address_precision(pfi, num);
+		print_address_precision(pfi, num, prcsn);
 	}
 }
 
 void				print_address(t_pf_item *pfi, long int num)
 {
 	char			*tmp;
+	int				prcsn;
 
 	pfi->bytes += 2;
+	prcsn = pfi->precision;
 	tmp = ft_address_to_hex(num);
 	if (pfi->flags->zero)
 		ft_putstr("0x");
 	if (pfi->field_w > 0)
 	{
 		if (pfi->precision > pfi->field_w)
-			print_address_precision(pfi, tmp);
+			print_address_precision(pfi, tmp, prcsn);
 		else
-			print_address_field_w(pfi, tmp);
+			print_address_field_w(pfi, tmp, prcsn);
 	}
 	else if (pfi->precision > 0)
-		print_address_precision(pfi, tmp);
+		print_address_precision(pfi, tmp, prcsn);
 	else if (pfi->lenmods->p && pfi->precision == 0)
 		ft_putstr("0x");
 	else
